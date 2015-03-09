29053bc77a53a97cc18369862d0cc63e9a63e23a
Jim Albert <jim@radxa.com>
2015-03-09 11:32:28 +0800

    remove clean function

--- scripts/mkupdate.sh
+++ scripts/mkupdate.sh
@@ -19,20 +19,9 @@ U_BOOT_BIN=
 
 function init()
 {
-	rm -f "$ROCKDEV_DIR/"*.img "$ROCKDEV_DIR/"*.bin
 	[ ! -s "${UBOOT_SRC}/RK3188Loader_miniall.bin" ] || cp -v "${UBOOT_SRC}/"RK3188Loader_miniall.bin $ROCKDEV_DIR
 	[ ! -s "${UBOOT_SRC}/uboot.img" ] || cp -v "${UBOOT_SRC}/"uboot.img $ROCKDEV_DIR
 	[ ! -s "${UBOOT_SRC}/RK3288UbootLoader_V2.19.06.bin" ] || cp -v "${UBOOT_SRC}/"RK3288UbootLoader_V2.19.06.bin $ROCKDEV_DIR
-	if [ ! -e "$ROOTFS_DST/rootfs.ext4" ]; then
-		if [ -e "$ROOTFS_SRC/$BOARD-rootfs.ext4" ]; then
-			cp $ROOTFS_SRC/$BOARD-rootfs.ext4 $ROOTFS_DST/rootfs.ext4
-		else
-			echo "rootfs.ext4 does not exist!"
-			exit 1
-		fi
-	fi
-	cat parameter/$BOARD-parameter > $BOARD/rockdev/parameter
-	cat package-file/$BOARD-package-file > $BOARD/rockdev/package-file
 }
 
 function pack()
@@ -48,19 +37,5 @@ function pack()
 	echo -e "Image is at \033[1;36m${ROCKDEV_DIR}/${IMAGE}\033[00m"
 	cd - > /dev/null
 }
-
-function clean()
-{
-	if [ `ls ${ROCKDEV_DIR}/*.img | wc -l` -gt 5 ]; then
-		dir_size=`du -sh ${ROCKDEV_DIR} | awk '{print $1}'`
-		echo -e "\033[00;41mThe rockdev size: $dir_size\033[0m"
-		read -p "Do you want to clean it(y/n)?" result
-		if [ "$result" = 'y' ]; then
-			ls ${ROCKDEV_DIR}/*.img | grep -v ${IMAGE} | xargs rm -f
-			echo "clean done"
-		fi
-	fi
-}
 init
 pack
-clean

7d00f4a8e9e53d3374730dc785e4b4cf9214cf0e
Jim Albert <jim@radxa.com>
2015-03-09 11:31:08 +0800

    add copy parameter function

--- scripts/hwpack.sh
+++ scripts/hwpack.sh
@@ -10,15 +10,12 @@ die() {
 . ./.config
 
 HWPACK_DIR="${BOARD}/rockdev/${BOARD}_hwpack"
-ROCKDEV_DIR="${BOARD}/rockdev"
 OUT_DIR=rootfs/$BOARD-rootfs
 IMAEGPACK_SRC=$OUT_DIR/$IMAGE_NAME
 IMAGE_NAME=
 
 init() {
-	if [ ! -d "$OUT_DIR" ]; then
-		mkdir -p $OUT_DIR
-	fi
+	mkdir -p $OUT_DIR
 	IMAGE_NAME=$(basename $OUT_DIR/linaro*.tar.gz)
 	if [ "$IMAGE_NAME"x = "linaro*.tar.gz"x ]; then
 		wget -P $OUT_DIR $ROOTFS_URL
@@ -47,45 +44,33 @@ cp_debian_files() {
 create_hwpack() {
 	local hwpack="$1"
 	local rootfs_hwpack="$HWPACK_DIR/rootfs"
-	local kernel_hwpack="$HWPACK_DIR/kernel"
+	local parameter_hwpack="$HWPACK_DIR/parameter"
+	local bootimg_hwpack="$HWPACK_DIR/bootimg"
 	local bootloader_hwpack="$HWPACK_DIR/bootloader"
 	local f=
 
 	rm -rf "$HWPACK_DIR"
 
+	## rootfs
 	mkdir -p "$rootfs_hwpack/lib"
-	rm -rf rockdev
-	ln -s $ROCKDEV_DIR rockdev
-
 	cp_debian_files "$rootfs_hwpack"
 
+	## parameter
+	mkdir -p "$parameter_hwpack"
+	cp -r $PARAMETER "$parameter_hwpack/"
+
 	## kernel
-	mkdir -p "$kernel_hwpack"
-	cp -r "$KERNEL_SRC/arch/arm/boot/Image" "$kernel_hwpack/" >/dev/null 2>&1
-	cp -r "$KERNEL_SRC/arch/arm/boot/zImage" "$kernel_hwpack/" >/dev/null 2>&1
+	mkdir -p "$bootimg_hwpack"
+	cp -r "$ROCKDEV_DIR/Image/boot-linux.img" "$bootimg_hwpack/" >/dev/null 2>&1
 
 	## bootloader
 	mkdir -p "$bootloader_hwpack"
-	cp -r "$UBOOT_SRC/"*.bin "$bootloader_hwpack/"
-	[ -s "$UBOOT_SRC/uboot.img" ] || cp -r "$UBOOT_SRC/".img "$bootloader_hwpack/" 1> /dev/null
+	cp -r "$UBOOT_SRC/"$U_BOOT_BIN "$bootloader_hwpack/"
 
 	## compress hwpack
 	cd "$HWPACK_DIR"
 	echo "waiting ..."
-	case "$hwpack" in
-	*.7z)
-		7z u -up1q0r2x1y2z1w2 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "$hwpack" .
-		;;
-	*.tar.bz2)
-		find . ! -type d | cut -c3- | sort -V | tar -jcf "$hwpack" -T -
-		;;
-	*.tar.xz)
-		find . ! -type d | cut -c3- | sort -V | tar -Jcf "$hwpack" -T -
-		;;
-	*)
-		die "Not supported hwpack format"
-		;;
-	esac
+	find . ! -type d | cut -c3- | sort -V | tar -jcf "$hwpack" -T -
 	cd - > /dev/null
 	mv $HWPACK_DIR/$BOARD-hwpack.tar.bz2 $ROCKDEV_DIR
 	rm -rf $HWPACK_DIR

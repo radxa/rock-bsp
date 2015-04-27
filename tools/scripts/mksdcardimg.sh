58f2882f88bd16dcf7fe6e9751a0e2677872a298
Jim Albert <jim@radxa.com>
2015-04-27 19:58:17 +0800

    fix rkcrc path

--- tools/scripts/mksdcardimg.sh
+++ tools/scripts/mksdcardimg.sh
@@ -3,17 +3,17 @@
 TODAY="`date +"%y-%m-%d"`"
 IMG="rock_lite_${TODAY}_sdcard.img"
 
-rkcrc -p parameter parameter.img
+tools/bin/rkcrc -p parameter parameter.img
 
 rm -rf $IMG
 dd if=/dev/zero of=$IMG bs=1M count=1950
 
-export START_SECTOR=65536
+export ROOTFS_SECTOR=65536
 sudo fdisk $IMG  << EOF
 n
 p
 1
-$START_SECTOR
+$ROOTFS_SECTOR
 
 w
 EOF
@@ -21,4 +21,4 @@ EOF
 sudo dd if=u-boot-sd.img of=$IMG conv=notrunc,sync seek=64
 sudo dd if=parameter.img of=$IMG conv=notrunc,sync seek=$((0x2000))
 sudo dd if=boot/boot-linux.img of=$IMG conv=notrunc,sync seek=$((0x2000+0x2000))
-sudo dd if=rootfs.img of=$IMG conv=notrunc,sync seek=65536
+sudo dd if=rootfs.img of=$IMG conv=notrunc,sync seek=$ROOTFS_SECTOR

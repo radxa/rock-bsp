97c8ffb062f53441fbec4456ceb91bfc8c46b1fa
Jim Robert <jim@radxa.com>
2015-08-27 20:53:55 +0800

    fix dd error

--- tools/scripts/mksdcardimg.sh
+++ tools/scripts/mksdcardimg.sh
@@ -18,7 +18,7 @@ w
 EOF
 
 #sudo dd if=u-boot-sd.img of=$IMG conv=notrunc,sync seek=64
-sudo dd if=rk_loader/sdboot_rk3188.img of=/dev/sdc skip=1 seek=1
+sudo dd if=rk_loader/sdboot_rk3188.img of=$IMG skip=1 seek=1
 sudo dd if=parameter.img of=$IMG conv=notrunc,sync seek=$((0x2000))
 sudo dd if=boot/boot-linux.img of=$IMG conv=notrunc,sync seek=$((0x2000+0x2000))
 sudo dd if=rootfs.img of=$IMG conv=notrunc,sync seek=$ROOTFS_SECTOR
